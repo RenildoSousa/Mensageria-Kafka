b705597c0c06897c330b72fa14d172759b52e412
renildosousa <renildosousa75@gmail.com>
2021-10-30 06:39:37 -0300

    Adiconando camada

--- src/main/java/br/com/renildo/ecommerce/EmailService.java
+++ src/main/java/br/com/renildo/ecommerce/EmailService.java
@@ -5,10 +5,13 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 public class EmailService {
     public static void main(String[] args) {
         var emailService = new EmailService();
-        var service = new KafkaService("ECOMMERCE_SEND_EMAIL", emailService::parse);
-        service.run();
+        try (var service = new KafkaService(EmailService.class.getSimpleName(),
+                "ECOMMERCE_SEND_EMAIL", emailService::parse)) {
+            service.run();
+        }
     }
-    private void parse(ConsumerRecord<String,String> record){
+
+    private void parse(ConsumerRecord<String, String> record) {
         System.out.println("______________________________________");
         System.out.println("Emviando email");
         System.out.println(record.key());
--- src/main/java/br/com/renildo/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/renildo/ecommerce/NewOrderMain.java
@@ -1,38 +1,22 @@
 package br.com.renildo.ecommerce;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(properties());
-        var value = "12331,131231,5242425424";
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        Callback callback = (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
+        try (var dispatcher = new KafkaDispatcher()) {
+            for (var i = 0; i < 10; i++) {
+
+                var key = UUID.randomUUID().toString();
+                var value = key + ",131231,5242425424";
+                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+
+                var email = "Bem vindo! Sua ordem está sendo processada!";
+                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
             }
-            System.out.println("Mensagem enviada " + data.topic() + ":::partition " + data.partition() + " / offset " + data.offset() + " timestamp " + data.timestamp());
-        };
-        var email = "Bem vindo! Sua ordem está sendo processada!";
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
-        producer.send(record, callback).get();
-        producer.send(emailRecord, callback).get();
+        }
     }
 
-    private static Properties properties() {
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return properties;
-    }
 
 }
